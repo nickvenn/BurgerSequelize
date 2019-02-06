20079aaacce4dffca7c6d8d1d2d16f86fd615b47
Nick Vennarucci <nickvenn@comcast.net>
2019-02-05 20:18:31 -0800

    first push

--- db/seeds.sql
+++
@@ -1,9 +0,0 @@
-USE burgers_db;
-select * from burgers
-
-INSERT INTO burgers 
-values(1,'cheeseburger', false);
-INSERT INTO burgers 
-values(2,'bacon burger', false);
-INSERT INTO burgers 
-values(3,'blue cheese burger', false);
